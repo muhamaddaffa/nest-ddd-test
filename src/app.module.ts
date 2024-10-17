f7897979d79f9bf1b3996086e6f8be5f76fc104d
Muhamad Daffa <daffafa09@gmail.com>
2024-10-17 23:02:11 +0700

    fea: add todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './shared/services/database-connection.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
